3106727996d59c628af309c33a33754fe25b3d89
Gabe <Carrollgbusiness@gmail.com>
2026-04-15 13:41:29 -0500

    string conversion , if statement to determine game winner

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -13,31 +13,30 @@ public class App {
         // Split the string into each part ex: home , visitor, 21, 9
         //how do i split a string?
         //what am i splitting on? a space,comma or semi colin etc.
-        System.out.println("------------------First Split------------------");
+
+
+
+
         // Splits score at the | first part is  left side of pipe second part is right side
         // tested with
         //System.out.println( "the first part is " + teamScore[0] );
         //System.out.println( "the second part is " + teamScore[1] );
         String[] teamScore = userInput.split("\\|");
-        System.out.println( "the first part is " + teamScore[0] );
-        System.out.println( "the second part is " + teamScore[1] );
-
-        System.out.println("------------------Team Split------------------");
-
         //splits the first part of the teamScore split at the ":"
         String[] team = teamScore[0].split(":");
-        System.out.println( "the first part is " + team[0] );
-        System.out.println( "the second part is " + team[1] );
-
-        System.out.println("------------------Score Split------------------");
-
         //splits the second part of the teamScore split at the ":"
         String[] score = teamScore[1].split(":");
-        System.out.println( "the first part is " + score[0] );
-        System.out.println( "the second part is " + score[1] );
 
 
+        //change score strings to int
+        int score1 = Integer.parseInt(score[0]);
+        int score2 = Integer.parseInt(score[1]);
 
-
+        // write if statement to determine which team won and display it
+        if (score1>score2){
+            System.out.println("winner: " + team[0]);
+        } else if (score2>score1) {
+            System.out.println("winner: " + team[1]);
+        }
     }
 }
